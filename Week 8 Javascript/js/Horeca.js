3845cff85a90b600acf26eeef7b233eeedffc47d
Willem <willem.zheng.xu@gmail,com>
2019-11-18 09:35:56 +0100

    commit

--- Week 8 Javascript/js/Horeca.js
+++ Week 8 Javascript/js/Horeca.js
@@ -15,9 +15,11 @@ var wijno = 0;
 var snackp = 0.5;//"P" achter snack geeft prijs aan
 var snacka = 0;//"A" achter snack geeft aantal aan
 var snacko = 0;//"O" achter snack geeft optelling aan
+var snackss
 
 while(bestelling!="stop") {
 	bestelling = prompt("Wat wil je bestellen?")
+	bestelling = bestelling.toLowerCase();
 		if (bestelling =="fris") {
 			frisa = prompt("Hoevel fris wil je hebben?")
 			friso = friso + parseInt(frisa)
@@ -33,7 +35,8 @@ while(bestelling!="stop") {
 		if (bestelling =="snack" || bestelling =="snacks") {
 			snacka = prompt("Hoeveel bitterballen wil je hebben? (Alleen keuzen uit 8 of 16)")
 			if (snacka =="8" || snacka =="16") {
-			snacko = snacko + parseInt(snacka)
+				snackss = prompt("Hoeveel schalen van " + snacka + " wil je hebben?")
+			snacko = snacko + parseInt(snacka) * parseInt(snackss)
 		} else {
 			alert("Je kunt alleen kiezen uit 8 of 16 bitterballen, je bestelling is niet toegevoegd")
 		}
@@ -48,12 +51,17 @@ function prijs() {
 	var snackpa = parseInt(snackp * snacko)
 	
 	var totaal = frispa + bierpa + wijnpa + snackpa;//rekent alles bij elkaar op
-
+	if (friso >0){
 	document.write("Prijs fris = &euro; " + frispa + "<br>");
+	}
+	if (biero >0){
 	document.write("Prijs bier = &euro; " + bierpa + "<br>");
+	}
+	if (wijno >0){
 	document.write("Prijs wijn = &euro; " + wijnpa + "<br>");
+	}
 	if (snacko =="8" || snacko =="16"){
-		document.write("Prijs bitterballen = &euro; " + snackpa + "<br>")
+		document.write("Prijs voor " + snackss + " schalen bitterballen = &euro; " + snackpa + "<br>")
 	}
 	document.write("De totale bedrag = &euro; " + totaal)
 }
@@ -62,5 +70,7 @@ function prijs() {
 if (bestelling =="stop") {
 	prijs();
 }
+
+
 
 
